d0d56d626eaf2d9e979335eb63ee0e797b76262e
franklinlugo <franklinlugo2805@gmail.com>
2020-10-05 09:09:36 -0300

    update tests

--- src/redux/actions.test.js
+++ src/redux/actions.test.js
@@ -1,5 +1,5 @@
 import * as TYPES from './types';
-import { onFetchStart, onFetchError, onFetchSuccess, onFetch } from './actions';
+import { onFetchStart, onFetchError, onFetchSuccess, onFetch, removePostById, setActivePost } from './actions';
 import { getTopPost as getTopPostMock } from '../services';
 
 jest.mock('../services');
@@ -14,6 +14,16 @@ test('Should return onFetchError action type ', () => {
 
 test('Should return onFetchSuccess action type ', () => {
   expect(onFetchSuccess([])).toEqual({ type: TYPES.FETCH_DATA_SUCCESS, payload: [] });
+});
+
+test('Should return removePostById action type ', () => {
+  const id = ' 1234';
+  expect(removePostById(id)).toEqual({ type: TYPES.REMOVE_POST_BY_ID, payload: id });
+});
+
+test('Should return setActivePost action type ', () => {
+  const postData = { id: '001', title: 'post 001' };
+  expect(setActivePost(postData)).toEqual({ type: TYPES.SET_ACTIVE_POST, payload: postData });
 });
 
 test('Should handle onFetch action type ', async () => {
--- src/redux/reducer.test.js
+++ src/redux/reducer.test.js
@@ -1,6 +1,6 @@
 import { reducer } from './reducer';
 import { initialState } from './initialState';
-import { onFetchStart, onFetchError, onFetchSuccess } from './actions';
+import { onFetchStart, onFetchError, onFetchSuccess, removePostById, setActivePost } from './actions';
 
 test('Should return initial values', () => {
   expect(reducer(undefined, {})).toEqual(initialState);
@@ -11,6 +11,7 @@ test('Should handle fetch data type', () => {
     posts: [],
     loading: true,
     error: null,
+    activePost: null,
   });
 });
 
@@ -20,6 +21,7 @@ test('Should handle fetch data success type', () => {
     posts: [...mockData],
     loading: false,
     error: null,
+    activePost: null,
   });
 });
 
@@ -28,5 +30,37 @@ test('Should handle fetch data error type', () => {
     posts: [],
     loading: false,
     error: true,
+    activePost: null,
   });
+});
+
+test('Should handle remove post by id type', () => {
+  const id = '2222';
+  const initial = {
+    ...initialState,
+    posts: [
+      { id: '1111', title: '1111' },
+      { id: '2222', title: '2222' },
+      { id: '3333', title: '3333' },
+      { id: '4444', title: '4444' },
+    ],
+  };
+  const spectedState = {
+    ...initialState,
+    posts: [
+      { id: '1111', title: '1111' },
+      { id: '3333', title: '3333' },
+      { id: '4444', title: '4444' },
+    ],
+  };
+  expect(reducer(initial, removePostById(id))).toEqual(spectedState);
+});
+
+test('Should handle set active post type', () => {
+  const postData = { id: '1111', title: '1111' };
+  const spectedState = {
+    ...initialState,
+    activePost: { ...postData },
+  };
+  expect(reducer(initialState, setActivePost(postData))).toEqual(spectedState);
 });
